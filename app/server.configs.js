e6aafcf7caec1d7b34c6abb14439afaddb485e75
ClementPoueyto <poueyto.clement@gmail.com>
2020-06-29 21:46:25 +0200

    test deployment

--- app/server.configs.js
+++ app/server.configs.js
@@ -1,5 +1,5 @@
 const PORT  = 9428
-const serverUrl = () => { return "http://localhost:"+PORT+"/" }
-const DB = "mongodb://localhost/memories"
+const serverUrl = () => { return "https://memories.osc-fr1.scalingo.io/" }
+const DB = "mongodb://admindb:adminMemories@34183965-0160-4f9e-b1b6-ff88f03fbd48.memories-3384.mongo.dbs.scalingo.com:32389/memories-3384?replicaSet=memories-3384-rs0&ssl=true"
 
 module.exports = { serverUrl, PORT, DB }

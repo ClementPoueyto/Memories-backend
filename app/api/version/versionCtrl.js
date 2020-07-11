115ffedd29e0d22617b5c536dcbbcf082409eb20
ClementPoueyto <poueyto.clement@gmail.com>
2020-07-11 14:54:28 +0200

    v 1.2.0

--- app/api/version/versionCtrl.js
+++ app/api/version/versionCtrl.js
@@ -1,5 +1,5 @@
 module.exports = {
     getVersion  :function (req, res) {
-        return res.status(200).send(['1.1.0','1.0.1','1.1.1'])
+        return res.status(200).send(['1.2.0'])
     }
 }
--- app/server.configs.js
+++ app/server.configs.js
@@ -1,8 +1,8 @@
-//const PORT  = 9428
-const PORT  =55924
+const PORT  = 9428
+//const PORT  =55924
 const serverUrl = () => { return "https://localhost:"+PORT+"/" }
-//const DB = "mongodb://localhost/memories"
-const DB="mongodb://admin:mencelt123@ds255924.mlab.com:55924/heroku_w72qz2t4"
+const DB = "mongodb://localhost/memories"
+//const DB="mongodb://admin:mencelt123@ds255924.mlab.com:55924/heroku_w72qz2t4"
 const AWS_SECRET_ACCESS ="NNl12SQDJmV30wS8wCvwyiEndgvFvG7zlwGPzZJj";
 
 const AWS_ACCESS_KEY="AKIAI6YJ3WT57NZT7FQQ";
